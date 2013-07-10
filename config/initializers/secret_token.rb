aa7e8e14f9a9e584881e66eb5dcc8399828ae14d
Wesley Carr <surfswell17@gmail.com>
2013-07-10 15:26:56 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviez::Application.config.secret_token = '1a99736a2e2f1160d0652b6ce6b82a831b80eaab0450dccb709eae8b98fc8e28e23b32eac7aca04446583d736ada43d3ce86531ae7abea3eb2205d4404e3bce6'
